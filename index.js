f536256e5fdb06bc0a6af092ee709111309d670f
s96116157 <s96116157@gmail.com>
2018-04-27 15:21:25 +0800

    fix

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.get('/', function (req, res) {
 
 app.post('/linewebhook', linebotParser);
 
+
 bot.on('message', function (event) {
     switch (event.message.type) {
         case 'text':
@@ -24,7 +25,7 @@ bot.on('message', function (event) {
                 case '早安':
                     event.source.profile().then(function (profile) {
                         return event.reply([
-                            { type: 'text', text: '早安！' + profile.displayName + ' 平安喜樂 ！' },
+                            { type: 'text', text: '早安！' + profile.displayName + ' 平安喜樂！' },
                             {
                                 type: 'image',
                                 originalContentUrl: 'https://i.imgur.com/0dBUGqP.jpg?1',
@@ -34,24 +35,23 @@ bot.on('message', function (event) {
                         //return event.reply('哈囉！' + profile.displayName + ' ' + profile.userId);
                     });
                     break;
-                case 'Time2':
+                case 'Meeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee':
                     event.source.profile().then(function (profile) {
-                        var dt = new Date().getHours() + 8;
-
                         return event.reply([
-                            { type: 'text', text: '現在時間：' + dt + ' 點！' }
+                            { type: 'text', text: '哈囉！' + profile.displayName + ' 您好！' },
+                            {
+                                type: 'text', text: '現在時間是：' + Get_Time + ' 點！'
+                                //type: 'image',
+                                //originalContentUrl: 'https://imgur.com/gallery/0dBUGqP',
+                                //previewImageUrl: 'https://imgur.com/gallery/0dBUGqP'
+                            }
                         ]);
-                    })
-                    break;
-                case 'Time':
-                    event.source.profile().then(function (profile) {
+                        //return event.reply('哈囉！' + profile.displayName + ' ' + profile.userId);
                     });
                     break;
-                case 'Group':
+                case 'Memberrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr':
                     event.source.member().then(function (member) {
-                        //var sticker = new LINEBot.getGroupMember(event.source.groupId);
-                        //return event.reply(JSON.stringify(member));
-                        return event.reply(member);
+                        return event.reply(JSON.stringify(member));
                     });
                     break;
                 case 'Pictureeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee':
@@ -79,7 +79,7 @@ bot.on('message', function (event) {
                 case 'Multicast':
                     //bot.push(['U17448c796a01b715d293c34810985a4c', 'Cba71ba25dafbd6a1472c655fe22979e2'], 'Multicast!');
                     break;
-                case 'Confirm':
+                case 'Confirmmmmmmmmmmmmmmmmmmmmmmmmmmmmm':
                     event.reply({
                         type: 'template',
                         altText: 'this is a confirm template',
@@ -100,15 +100,8 @@ bot.on('message', function (event) {
                     break;
                 case 'bye':
                     event.source.member().then(function (member) {
-
-                        if (event.isGroupEvent() == true) {
-                            bot.leaveGroup(event.getGroupId()); //退出群組
-                        }
-                        else if (event.isRoomEvent() == true) {
-                            bot.leaveRoom(event.getRoomId()); //退出聊天室
-                        }
-
                         return event.reply([
+                            { type: 'text', text: JSON.stringify(member) },
                             { type: 'text', text: '不要這樣嘛～' }
                         ])
                     });
@@ -142,3 +135,8 @@ bot.on('message', function (event) {
 app.listen(process.env.PORT || 80, function () {
     console.log('LineBot is running.');
 });
+
+function Get_Time() {
+    var dt = new Date();
+    return dt.getDate();
+}
