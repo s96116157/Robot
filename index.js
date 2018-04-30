0e6ae9caa8e4d79000b3bd557fe74fa308812337
s96116157 <s96116157@gmail.com>
2018-04-30 18:38:00 +0800

    1

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function _getJSON() {
    // var url = "https://script.google.com/macros/s/AKfycbzGYm641yIGwZ0Vb9a_jXiKoVOooYZfoXWJAprggzC4G0FbR6YM/exec?id=1X5Mtln-MYBhyBRn0RveNOXCkb32A4VTzht1AIGkNvdU&sheet=From_002"
     var url = "https://script.googleusercontent.com/macros/echo?user_content_key=2KtuSbS26jWQtTCeVVCLxRogMNcs9Q3J6rZG2Wyplr8DVqdCtWjDwvFeNKxgK6-7ijZ3E3ezcRaPlPsVDdBDz04GvLV9SLn8OJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHa9kwxyVAdOctIYg4SBSdu9k-m3IS2J5xHgKvKyWLLaSDbNf0LmFKQpTt56_yJz549EKQLumG9TUsY7O6EXi9VUxCG0BDbUzq3s9njy9H1eM991tXI9T9B6smYauuRThZH_FtbzwwXe895tYrEq1gHrFvqrVIJO7UzQ&lib=M8N87m5Wgvt1ePBAUHd3fUw2OPgjxcCW0";
     var url2 = "http://opendata2.epa.gov.tw/AQX.json";
-    var txt = "[{SYSID:2,Form_ID:1},{SYSID:2,Form_ID:1}]";
+    var txt = "https://github.com/s96116157/Robot/blob/master/echo.json";
     console.log('Function Start... v.003');
     getJSON(txt, function (error, response) {
         console.log(response);
