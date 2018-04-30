39196b4ff6f941263d08c3c8e0df24c48c824673
s96116157 <s96116157@gmail.com>
2018-04-30 16:34:59 +0800

    1

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ var bot = linebot({
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
+var pm = [];
+_getJSON();
+
 var app = express();
 
 app.get('/', function (req, res) {
@@ -18,3 +21,16 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT || 80, function () {
     console.log('LineBot is running.');
 });
+
+function _getJSON() {
+    console.log('Function Start...');
+    getJSON('http://opendata2.epa.gov.tw/AQX.json', function (error, response) {
+        response.forEach(function (e, i) {
+            pm[i] = [];
+            pm[i][0] = e.SiteName;
+            pm[i][1] = e['PM2.5'] * 1;
+            pm[i][2] = e.PM10 * 1;
+        });
+    });
+    console.log('Function End...');
+}
